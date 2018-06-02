e9a2d27f25ed5e6c98bcc60fc52322758491f20a
Yathartha Goel <yatharthagoel@gmail.com>
2018-06-02 00:02:36 -0700

    Added Licenses

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
-from setuptools import setup
-setup(
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
     name = 'MusixLI',
     version = '1.0',
-    description = 'A command line application that lets you search for different artists, albums, songs and their lyrics with just entering certain commands.',
+    description = 'A command line application that lets you search for different artists, albums, songs and their lyrics.',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     author = 'Yathartha Goel',
+    license = 'MIT License',
+    platforms = 'Cross Platform',
+    packages = setuptools.find_packages(),
     py_modules = ['musixli_main'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
     install_requires = [
         'Click',
         'pymusixmatch',
