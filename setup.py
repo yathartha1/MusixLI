16224bd0737e178a654bf010752e6ece97ebc9e3
Yathartha Goel <yatharthagoel@gmail.com>
2018-06-01 23:33:52 -0700

    Updated Setup

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name = 'MusixLI',
     version = '1.0',
+    description = 'A command line application that lets you search for different artists, albums, songs and their lyrics with just entering certain commands.',
+    author = 'Yathartha Goel',
     py_modules = ['musixli_main'],
     install_requires = [
         'Click',
